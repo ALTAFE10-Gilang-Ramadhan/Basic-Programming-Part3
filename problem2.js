9aa6c9440be631e5a395b97c87481f7dfe341315
Gilang Ramadhan <gilangr1440@gmail.com>
2022-11-09 20:38:32 +0700

    tugas js part 3

---
+++ problem2.js
@@ -0,0 +1,14 @@
+function joinArrayRemoveDuplicate(arrayA, arrayB) {
+  const join = arrayA.concat(arrayB);
+  const newA = [...new Set(join)];
+  return newA;
+}
+// Test cases
+console.log(joinArrayRemoveDuplicate(['apel', 'anggur'], ['lemon', 'leci', 'nanas']));
+// ["apel", "anggur", "lemon", "leci", "nanas"]
+
+console.log(joinArrayRemoveDuplicate(['samsung', 'apple'], ['apple', 'sony', 'xiaomi']));
+// ["samsung", "apple", "sony", "xiaomi"]
+
+console.log(joinArrayRemoveDuplicate(['football', 'basketball'], ['basketball', 'football']));
+// [“football”, “basketball”]
